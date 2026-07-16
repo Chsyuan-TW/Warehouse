eb280130517d75388fcb6a55fdaf8b05f0f4d973
Chsyuan-TW <syuan.chang@gmail.com>
2026-07-16 13:32:32 +0800

    Add files via upload

---
+++ database.js
@@ -0,0 +1,17 @@
+// 🌟 這是您的獨立資料庫檔案 (database.js) 🌟
+// 請將您的 Excel 資料庫轉檔成以下 JSON 格式。
+// 每次轉完檔，直接「覆蓋此檔案」即可，完全不需要去修改 index.html！
+
+window.warehouseDatabase = [
+
+    {"id": "113A180317", "size": "13", "loc": "1-2", "far_img": "images/mark_far.jpg", "near_img": "images/mark_shelf1.jpg", "detail_img": "", "pointer_img": "images/113A180317.jpg", "note": "第一排左手邊數來第2顆"},
+    {"id": "114F000861", "size": "75", "loc": "大表區", "far_img": "images/mark_far.jpg", "near_img": "images/mark_large.jpg", "detail_img": "images/mark_114F000861.jpg", "pointer_img": "", "note": "左手邊從牆壁數來第2顆"},
+    {"id": "114E000766", "size": "50", "loc": "大表區", "far_img": "images/mark_far.jpg", "near_img": "images/mark_large.jpg", "detail_img": "images/mark_114E000766.jpg", "pointer_img": "", "note": ""},
+    {"id": "113A183804", "size": "13", "loc": "1-2", "far_img": "images/mark_far.jpg", "near_img": "images/mark_shelf1.jpg", "detail_img": "", "pointer_img": "", "note": ""},
+    {"id": "113A183875", "size": "13", "loc": "1-2", "far_img": "images/mark_far.jpg", "near_img": "images/mark_shelf1.jpg", "detail_img": "", "pointer_img": "", "note": ""},
+    {"id": "114A083644", "size": "13", "loc": "1-3", "far_img": "images/mark_far.jpg", "near_img": "images/mark_shelf1.jpg", "detail_img": "", "pointer_img": "", "note": ""},
+    {"id": "109B391317", "size": "20", "loc": "2-1", "far_img": "images/mark_far.jpg", "near_img": "images/mark_shelf2.jpg", "detail_img": "", "pointer_img": "", "note": ""},
+    {"id": "112B360101", "size": "20", "loc": "2-2", "far_img": "images/mark_far.jpg", "near_img": "images/mark_shelf2.jpg", "detail_img": "", "pointer_img": "", "note": ""},
+    {"id": "110B069112", "size": "20", "loc": "2-3", "far_img": "images/mark_far.jpg", "near_img": "images/mark_shelf2.jpg", "detail_img": "", "pointer_img": "", "note": ""},
+
+];
